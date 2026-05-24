f9f56dc86338950536b2a22de9a39293be7bfbb6
camil2000 <mariacamillapra18@gmail.com>
2026-05-24 00:23:12 -0500

    commit Reportable

--- Modules/interfaces.py
+++ Modules/interfaces.py
@@ -1,1 +1,8 @@
-# Código Camila:
+from abc import ABC, abstractmethod
+from typing import Protocol, runtime_checkable
+
+@runtime_checkable
+class Reportable(Protocol):
+    def obtener_reporte(self) -> str:
+        ...
+
