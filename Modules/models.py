9d375685161c98363804abf493c6236d1f83dc0a
camil2000 <mariacamillapra18@gmail.com>
2026-05-24 00:07:35 -0500

    commit Sector

--- Modules/models.py
+++ Modules/models.py
@@ -1,1 +1,17 @@
-# Código Camila:
+from dataclasses import dataclass
+from interfaces import GameObject
+
+@dataclass
+class Sector:
+    nombre: str
+    dificultad: int
+
+class Item(GameObject):
+    def __init__(self, nombre: str, potencia: int, precio: int = 0):
+        super().__init__(nombre)
+        self.potencia: int = potencia
+        self.precio: int = precio
+
+    def obtener_reporte(self) -> str:
+        return f"[ITEM] {self.nombre} (Poder: {self.potencia})"
+
