a9ae922a586ee81490e5b629d2fae0d3bdda76ce
camil2000 <mariacamillapra18@gmail.com>
2026-05-24 00:13:23 -0500

    commit Actor

--- Modules/models.py
+++ Modules/models.py
@@ -15,3 +15,14 @@ class Item(GameObject):
     def obtener_reporte(self) -> str:
         return f"[ITEM] {self.nombre} (Poder: {self.potencia})"
 
+class Actor(GameObject):
+    def __init__(self, nombre: str, integridad: int):
+        super().__init__(nombre)
+        self.integridad: int = integridad
+
+    def esta_vivo(self) -> bool:
+        return self.integridad > 0
+
+    @abstractmethod
+    def obtener_reporte(self) -> str:
+        pass
